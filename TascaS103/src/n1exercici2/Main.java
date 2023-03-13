7ab5cafd83ba1c7c1821c512c8a59d58cbb450f8
KaidenTheCopyCat <helenagarciaviles@gmail.com>
2023-03-13 13:06:29 +0100

    N1Ex2

--- TascaS103/src/n1exercici2/Main.java
+++ TascaS103/src/n1exercici2/Main.java
@@ -1,5 +1,11 @@
 package n1exercici2;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -11,6 +17,34 @@ Crea un segon List<Integer> i insereix a la segona llista els elements de la pri
 Empra els objectes ListIterator per a llegir els elements de la primera llista i inserir-los en la segona.
 		 */
 
+		
+		List<Integer> llista1 = new ArrayList<>();
+		
+		llista1.add(1);
+		llista1.add(2);
+		llista1.add(3);
+		
+		ListIterator<Integer> iterador = llista1.listIterator();
+		
+		while (iterador.hasNext()) {
+			System.out.println(iterador.next());
+		}
+		
+		//cursor de l'iterador 'fora' de la llista
+		System.out.println("----");
+		List<Integer> llista2 = new ArrayList<>();
+		
+		while (iterador.hasPrevious()) {
+			llista2.add(iterador.previous());
+		}
+		
+		//demostracio llista2 es llista1 en ordre invers
+		ListIterator<Integer> iterador2 = llista2.listIterator();
+		
+		while (iterador2.hasNext()) {
+			System.out.println(iterador2.next());
+		}
+		
 	}
 
 }
